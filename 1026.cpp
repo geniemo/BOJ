1e154511752dcb13d418050c067d643593ffe3c4
jiweon_home <kor98won@gmail.com>
2021-01-01 01:49:25 +0900

    보물

---
+++ 1026.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main(void)
+{
+    int N;
+    int S = 0;
+    scanf(" %d", &N);
+    vector<int> v1;
+    vector<int> v2;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        scanf(" %d", &num);
+        v1.push_back(num);
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        scanf(" %d", &num);
+        v2.push_back(num);
+    }
+    // sort v1 ascending order
+    sort(v1.begin(), v1.end());
+    // sort v2 descending order
+    sort(v2.begin(), v2.end(), greater<int>());
+    for (int i = 0; i < N; i++)
+        S += (v1[i] * v2[i]);
+    printf("%d", S);
+    return 0;
+}
