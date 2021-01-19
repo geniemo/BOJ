727fe9e28ab1d5dd582f7b45a2b8c41f94a80ed8
jiweon <kor98won@gmail.com>
2021-01-19 16:47:01 +0900

    준오는 조류혐오야

--- 14647.cpp
+++ 14647.cpp
@@ -26,7 +26,7 @@ int main(void)
                 {
                     sum++;
                     row[i]++;
-                    col[j]++;
+                    col[j]++; 
                 }
                 num /= 10;
             }
