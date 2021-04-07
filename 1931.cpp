c261b01d9aa2510ff7fd092e72fd0f813ec7cc92
jiweon <kor98won@gmail.com>
2021-04-07 13:48:38 +0900

    회의실 배정

--- 1931.cpp
+++ 1931.cpp
@@ -1,16 +1,12 @@
 #include <bits/stdc++.h>
+#define PAIR    pair<int, int>
+#define ST      second
+#define EN      first
 using namespace std;
 
 int N;
-int st_org[100005];
-pair<int, int> en[100005];
+PAIR t[100005];
 int cnt;
-
-bool cmp(pair<int, int>& a, pair<int, int>& b) {
-    if (a.first == b.first)
-        return st_org[a.second] < st_org[b.second];
-    return a.first < b.first;
-}
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -19,16 +15,15 @@ int main(void) {
 
     cin >> N;
     for (int i = 0; i < N; i++) {
-        cin >> st_org[i] >> en[i].first;
-        en[i].second = i;
+        cin >> t[i].ST >> t[i].EN;
     }
-    sort(en, en + N, cmp); // 회의가 끝난 시간 기준 오름차순 정렬, 끝난시간이 같다면 시작한 시간 기준 오름차순 정렬
+    sort(t, t + N); // 회의가 끝난 시간 기준 오름차순 정렬, 끝난시간이 같다면 시작한 시간 기준 오름차순 정렬
     int endTime = -1; // 저번 회의가 끝난 시간
     for (int i = 0; i < N; i++) {
-        // 저번 회의가 끝난 시간이 en[i]의 시작시간보다 이르다면 카운트
-        if (endTime <= st_org[en[i].second]) {
+        // 저번 회의가 끝난 시간이 t[i]의 시작시간보다 이르다면 카운트
+        if (endTime <= t[i].ST) {
             cnt++;
-            endTime = en[i].first;
+            endTime = t[i].EN;
         }
     }
     cout << cnt;
