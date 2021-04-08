d58829cb7a55ebfc79cf7cd87eafc9711e0ade88
jiweon <kor98won@gmail.com>
2021-04-08 10:47:45 +0900

    회의실 배정

--- 12865.cpp
+++ 12865.cpp
@@ -17,7 +17,6 @@ int main(void) {
     cin >> N >> K;
     for (int i = 1; i <= N; i++)
         cin >> stuff[i].W >> stuff[i].V;
-    //sort(stuff, stuff + N + 1);
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= K; j++) {
             if (stuff[i].W <= j)
